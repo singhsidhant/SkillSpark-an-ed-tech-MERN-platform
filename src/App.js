5ada703fc274857f59a24a64fc3b828552e0eade
Sidhant Singh <ns865723@gmail.com>
2023-11-27 07:22:51 +0530

    feat:section 1 of Homepage completed which include video code and course card etc part

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import {Route, Routes} from "react-router-dom"
 
+//pages Import 
+import Home from "./pages/Home"
 function App() {
   return (
-    <main class="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 class="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p class="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="https://bit.ly/3wsmzTy" alt="meme" class="mx-auto" />
-    </main>
+    //Main Page where all the section are stored
+    <div className="flex min-h-screen w-screen flex-col bg-richblack-900 font-inter">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   );
 }
 
